fa2121ee99e056f3ed33c810a8488e4a627aacba
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-20 05:49:45 -0500

    more styles

--- tetris/src/Components/Styles/StyledCell.js
+++ tetris/src/Components/Styles/StyledCell.js
@@ -3,4 +3,6 @@ import styled from 'styled-component';
 export const StyledCell = styled.div`
   width: auto;
   background: #000000;
+  border: ${props => props.type === 0 ? '0px solid' : '4px solid'};
+  border-bottom-color: 
 `
