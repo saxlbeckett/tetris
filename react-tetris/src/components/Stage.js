4a8b9d4f199c90c74a9740aa875a12cb26b086a4
Scott <saxlbeckett@gmail.com>
2021-03-20 14:07:14 -0400

    Followed tutorial up through the making of tetrominos

--- react-tetris/src/components/Stage.js
+++ react-tetris/src/components/Stage.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Cell from './Cell'
 
 const Stage = ({stage}) => (
-  <div><Cell/></div>
+  <div>
+    {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]}/>))}
+  </div>
 )
 export default Stage
