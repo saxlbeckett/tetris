0ae2905dc9a1e4abcfc323bcfb6d7c7eb51a0943
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-20 15:41:28 -0500

    player hood ready

--- tetris/src/Hooks/usePlayer.js
+++ tetris/src/Hooks/usePlayer.js
@@ -3,8 +3,10 @@ import {randomTetromino} from '../StagePieces/tetrominos';
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
-    pos: {x: 0, y:0}
+    pos: {x: 0, y:0},
     tetrominos: randomTetromino().shape,
     collided: false
   });
+
+  return [player];
 }
