c755a522d9c5ca9c10fe336583b6cf422121095c
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-20 04:20:02 -0500

    stage outline

--- tetris/src/StagePieces/gameHelper.js
+++ tetris/src/StagePieces/gameHelper.js
@@ -1,1 +1,33 @@
 //arrays board - stage 
+export const STAGE_WIDTH = 12; 
+export const STAGE_HEIGHT = 20;
+
+export const createStage = () => {
+  Array.from(Array(STAGE_HEIGHT), () => {
+    new Array(STAGE_WIDTH).fill([0, 'clear'])
+  })
+} 
+
+//VISUAL OF WHAT THIS IS DOING 
+// | = STAGE HEIGHT array 
+// _ = STAGE WIDTH array 
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
+// |_ _ _ _ _ _ _ _ _ _ _ _|  
