3b9534092078c8adbff64228fc10c7bb4ab49d89
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-20 09:35:34 -0500

    remove unnned notes

--- tetris/src/Components/Stage.js
+++ tetris/src/Components/Stage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Cell from './Cell';
 
 const Stage = ({stage}) => {
-  console.log('in the stage : ',stage)// we got the array! 
+
   return(
     <div>The Stage aka Board
       {stage.map(row => row.map((cell, x) => { return <Cell key={x} type={cell[0]}/>}))}
