a3e73ff21f67e741cbb820b3ab9f1c797df3d262
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-20 15:22:52 -0500

    outline on call back

--- tetris/src/Components/StartButton.js
+++ tetris/src/Components/StartButton.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import {StyledStartButton} from './Styles/StyledStartButton'
 
 //do we also want to use this as the reset button? pass in, "if game over, restart?" Or do we want to create a separate component? 
+
 const StartButton = ({callback}) => (
-  <div>Start Game</div>
+  <StyledStartButton onClick={callback} >Start Game</StyledStartButton>
 )
 
 export default StartButton;
