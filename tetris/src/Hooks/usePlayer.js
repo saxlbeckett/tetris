faae42a19aca2f5b75c8013ef67c46e4ff380b2c
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-20 15:41:00 -0500

    state set for player

--- tetris/src/Hooks/usePlayer.js
+++ tetris/src/Hooks/usePlayer.js
@@ -2,5 +2,9 @@ import {useState} from 'react';
 import {randomTetromino} from '../StagePieces/tetrominos';
 
 export const usePlayer = () => {
-  
+  const [player, setPlayer] = useState({
+    pos: {x: 0, y:0}
+    tetrominos: randomTetromino().shape,
+    collided: false
+  });
 }
