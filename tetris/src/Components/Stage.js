a85b282c9d33ab5b5d6f0da1b15ef0a6aa3b8888
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-20 08:59:02 -0500

    stage map not working still

--- tetris/src/Components/Stage.js
+++ tetris/src/Components/Stage.js
@@ -2,15 +2,10 @@ import React from 'react';
 import Cell from './Cell';
 
 const Stage = ({stage}) => {
-  console.log(stage)// is coming across as undefined right now
-  let cells = stage.map((row) => {
-    row.map((cell, x) => {
-      <Cell key={x} type={cell[0]}/>
-    })
-  })
+  console.log('in the stage : ',stage)// we got the array! 
   return(
     <div>The Stage aka Board
-      {cells}
+      {stage.map(row => row.map((cell, x) => { <Cell key={x} type={cell[0]}/>}))}
     </div>
   )
 }
