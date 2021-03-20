886b218fab1786162fd4fee374d4c3d3b6d3ab92
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-20 05:55:49 -0500

    add shade on block

--- tetris/src/Components/Styles/StyledCell.js
+++ tetris/src/Components/Styles/StyledCell.js
@@ -4,5 +4,8 @@ export const StyledCell = styled.div`
   width: auto;
   background: #000000;
   border: ${props => props.type === 0 ? '0px solid' : '4px solid'};
-  border-bottom-color: 
+  border-bottom-color: rgba(${props => props.color}, 0.1);
+  border-right-color: rgba(${props => props.color}, 1);
+  border-left-color: rgba(${props => props.color}, 0.3);
+  border-top-color: rgba(${props => props.color}, 1);
 `
