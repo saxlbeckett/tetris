16fa8276877038f9a06bb374d596f52ce589fe9c
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-20 22:11:23 -0500

    update reset player hook infor

--- tetris/src/Hooks/usePlayer.js
+++ tetris/src/Hooks/usePlayer.js
@@ -1,4 +1,5 @@
-import {useState} from 'react';
+import {useState, useCallback} from 'react';
+import { STAGE_WIDTH } from '../StagePieces/gameHelper';
 import {randomTetromino} from '../StagePieces/tetrominos';
 
 export const usePlayer = () => {
@@ -8,13 +9,21 @@ export const usePlayer = () => {
     collided: false
   });
 
-  const updatePlayerPos = (({x, y, collided}) => {
+  const updatePlayerPos = ({x, y, collided}) => {
     //set state
     setPlayer(prev => ({
       ...prev,
       pos: {x:(prev.pos.x +=x), y:(prev.pos.y +=y)},
       collided,
     }))
+  }
+//need to use call back here to prevent infinite loop 
+  const resetPlayer = useCallback(() => {
+    setPlayer({
+      pos: {x: STAGE_WIDTH/2-2,y: 0},
+      tetrominos: randomTetromino().shape,
+      collided: false
+    })
   })
 
   return [player];
