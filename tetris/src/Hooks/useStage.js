75fea61020de064ff1e48a2ba173c832f19d1039
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-20 22:23:53 -0500

    update made to movement on shapes

--- tetris/src/Hooks/useStage.js
+++ tetris/src/Hooks/useStage.js
@@ -11,6 +11,23 @@ export const useStage = (player, resetPlayer) => {
       const newStage = prevStage.map(row => {
         row.map(cell => (cell[1] === 'clear' ? [0,'clear'] : cell))
       })
+
+      //draw new shape
+        //go through the multi layers
+      player.tetromino.forEach(row, y => {
+        row.forEach((value, x) => {
+          if(value !== 0) {
+            newStage[y+ player.pos.y][x + player.pos.x] = [
+              value,
+              //merge is = collision 
+              //clear = delete on next render
+              `${player.collided ? 'merged' : 'clear'}`
+            ]
+          }
+        })
+      });
+
+      return newStage
     }
 
     setStage(prev => updateStage(prev))
