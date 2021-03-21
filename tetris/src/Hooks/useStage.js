753bd7c1c9bb994717127f87447b4098d7ed3760
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-20 22:18:17 -0500

    action use effect in stage

--- tetris/src/Hooks/useStage.js
+++ tetris/src/Hooks/useStage.js
@@ -6,8 +6,11 @@ export const useStage = (player, resetPlayer) => {
 
   //moving items
   useEffect(() => {
-    const updateStage = prevState => {
-      
+    const updateStage = prevStage => {
+      //update the stage for 1st attempt
+      const newStage = prevStage.map(row => {
+        row.map(cell => (cell[1] === 'clear' ? [0,'clear'] : cell))
+      })
     }
 
     setStage(prev => updateStage(prev))
