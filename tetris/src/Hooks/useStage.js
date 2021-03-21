ade68f40e36e7acbb0c719cde74795e91ed86a25
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-21 17:17:27 -0500

    stage is looking better, scope was wrong. need to work on infinate loop now

---
+++ tetris/src/Hooks/useStage.js
@@ -0,0 +1,36 @@
+import {useState, useEffect} from 'react';
+import {createStage} from '../StagePieces/gameHelper'
+
+export const useStage = (player, resetPlayer) => {
+  const [stage, setStage] = useState(createStage());
+  console.log(player)
+  useEffect(() => {
+    
+    const updateStage = prevStage => {
+
+      //set update the stage and reset it
+      const newStage = prevStage.map(row => 
+        row.map(cell => (cell[1] !== 'clear' ? [0, 'clear'] : cell)),   
+      );//the end of the mapping 
+
+      //draw my shapes
+      player.tetrominos.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if(value !== 0) {
+            newStage [y+player.pos.y][x+player.pos.x] = [
+              value,
+              `${player.collided ? 'merged' : 'clear'}`
+            ]
+          }
+        })
+      })
+      return newStage
+    }//end of updateStage
+
+    setStage(prev => updateStage(prev))
+
+    //dependency 
+  }, [player.collided, player.pos.x, player.pos.y, player.tetrominos])
+
+  return [stage, setStage];
+}
