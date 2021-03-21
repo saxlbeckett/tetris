700763cda061a44bdfd366e04551727e2dad02f6
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-20 22:04:26 -0500

    added update postion and reset player to state on player

--- tetris/src/Components/Tetris.js
+++ tetris/src/Components/Tetris.js
@@ -11,7 +11,7 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player] = usePlayer(); 
+  const [player, updatePlayerPost, resetPlayer] = usePlayer(); 
   const [stage, setStage] = useStage(player);
 
 console.log('re-render')
