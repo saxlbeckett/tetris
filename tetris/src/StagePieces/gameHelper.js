9630f0fb3dd8b9e665a8d8f0d97e3c00d2becca4
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-21 18:00:59 -0500

    move logic

--- tetris/src/StagePieces/gameHelper.js
+++ tetris/src/StagePieces/gameHelper.js
@@ -4,3 +4,26 @@ export const STAGE_HEIGHT = 20;
 
 export const createStage = () => Array.from(Array(STAGE_HEIGHT).fill(new Array(STAGE_WIDTH).fill([0, 'clear'])))
 
+export const checkCollision = (player, stage, {x: moveX, y:moveY}) => {
+
+  for(let y=0; y < player.tetrminos.length; y++) {
+    
+    for(let x=0; x < player.tetrminos.length; x++) {
+      //make sure we are in the tetromino cell 
+      if(player.tetrminos[y][x] !==0) {
+        //make sure we are within the game area, both height and width nor drop out the bottom
+        //set the section variable to verify the if collided and set the clear to merge
+
+        if(//make sure the next move it valid 
+          !stage[y + player.pos.y + moveY] || 
+          !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+          stage[y + player.pos.y + moveY][y + player.pos.x + moveX][1] !== 'clear' 
+        ) {
+          return true
+        }
+      }
+
+    }
+  }
+
+}
